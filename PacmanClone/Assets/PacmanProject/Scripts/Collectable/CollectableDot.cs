17d212d95763d63f21321ab19b5755af397f50a3
Frederic Levesque <flevesque124@gmail.com>
2020-02-03 19:38:35 -0500

    Prefabs Added Collectable and player move script added

---
+++ PacmanClone/Assets/PacmanProject/Scripts/Collectable/CollectableDot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableDot : MonoBehaviour
+{
+    public int m_Point = 10;
+    private PlayerStats m_PlayerStats;
+
+    private void Start()
+    {
+        m_PlayerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            m_PlayerStats.AddScorePoint(m_Point);
+            Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ PacmanClone/Assets/PacmanProject/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int m_PlayerLife = 3;
+    public int m_PlayerScore = 0;
+
+    //public Text m_TxtScore;
+    //public Text m_TxtLife;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+      //  m_TxtLife.text = m_PlayerLife.ToString();
+        //m_TxtScore.text = m_PlayerScore.ToString();
+    }
+
+    public void AddScorePoint(int scoreValue)
+    {
+        m_PlayerScore += scoreValue;
+        //m_TxtScore.text = m_PlayerScore.ToString();
+    }
+
+    public int GetScorePoint()
+    {
+        return this.m_PlayerScore;
+    }
+
+    public int GetLifeRemain()
+    {
+        return this.m_PlayerLife;
+    }
+
+    public void DecreaseLife()
+    {
+        if (this.m_PlayerLife >= 0)
+        {
+            this.m_PlayerLife--;
+            //m_TxtLife.text = m_PlayerLife.ToString();
+        }
+    }
+}
