025bbdb7cc55446425485ba6ad8bd7552d9abe2e
Frederic Levesque <flevesque124@gmail.com>
2020-02-03 20:10:13 -0500

    GameManager script added CollectableEnergize script added

---
+++ PacmanClone/Assets/PacmanProject/Scripts/Collectable/CollectableEnergize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableEnergize : MonoBehaviour
+{
+    public int m_Point = 50;
+    private PlayerStats m_PlayerStats;
+    private GameManager m_GameManager;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_PlayerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            m_PlayerStats.AddScorePoint(m_Point);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ PacmanClone/Assets/PacmanProject/Scripts/Game/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject[] m_DotGameObjectLists;
+    private int m_DotCollectableCount = 0;
+    private int m_DotEated = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_DotGameObjectLists = GameObject.FindGameObjectsWithTag("Dot");
+        m_DotCollectableCount = m_DotGameObjectLists.Length;
+        Debug.Log("" + m_DotCollectableCount);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void CountPlayerDotEaten()
+    {
+        if (m_DotEated <= m_DotCollectableCount)
+        {
+            m_DotEated++;
+        }
+        else
+        {
+            Debug.Log("All dot are eated finish the stage");
+        }
+    }
+}
